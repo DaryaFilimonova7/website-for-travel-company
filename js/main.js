86fb1b1d91317e104089ef9c3dbdef4a0b5a350c
Darya Filimonova <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-05-22 06:43:18 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+// Отправка формы бронирования тура
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('tour_booking_form')
+  form.addEventListener('submit', send_form)
+
+  async function send_form(e){
+    e.preventDefault();
+    let form_data = new FormData(form)
+    let response = await fetch('send_booking_form.php', {
+      method: 'POST',
+      body: form_data
+    })
+    if (response.ok){
+      form.classList.add('_sending')
+      let result = await response.json();
+      alert(result.message)
+      form.reset()
+      form.classList.remove('_sending')
+    }
+    else{
+      alert("Ошибка отправки формы.")
+      form.classList.remove('_sending')
+    }
+  }
+
+
+})
